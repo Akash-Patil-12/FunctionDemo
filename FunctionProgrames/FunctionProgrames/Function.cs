b8f61ff320e8b76128e1dfc4de748f96393276c3
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-19 11:44:08 +0530

    [Akash] Added : Function Feature is added

---
+++ FunctionProgrames/FunctionProgrames/Function.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionProgrames
+{
+    class Function
+    {
+        public void show()
+        {
+            Console.WriteLine("Function exicuted");
+        }
+    }
+}
---
+++ FunctionProgrames/FunctionProgrames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FunctionProgrames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Function obj = new Function();
+            obj.show();
+
+            Console.ReadLine();
+        }
+    }
+}
